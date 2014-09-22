27816ccfff36278b92f02df29a304e9f853241d3
eve-link <eve.link.1992@gmail.com>
2014-09-22 22:26:44 +0300

    elseif

--- harjutus-6.php
+++ harjutus-6.php
@@ -5,16 +5,22 @@
 	<title>Harjutus-6</title>
 </head>
 <body> 
-<h1>Loogika operatsioonid</h1>
-<h2>Tingimuslaused</h2>
+	<h1>Loogika operatsioonid</h1>
+	<h2>Tingimuslaused</h2>
 
-<?php 
-$a = 4;
-$b = 5;
-$a < $b;
-echo "4 on väiksem kui 5";
- ?>
- 
+	<?php 
+	$a = 4;
+	$b = 5;
+
+	if ($a < $b) {
+		echo "4 on väiksem kui 5";
+	} elseif ($a = $b) {
+		echo "4 ja 5 on võrdsed";
+	} elseif ($a > $b) {
+		echo "5 on suurem kui 4";
+	}
+	?>
+
 
 </body>
 </html> 
