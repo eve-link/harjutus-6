205578d878ce97a7a7609a51039777de00faf4f5
eve-link <eve.link.1992@gmail.com>
2014-09-22 22:19:02 +0300

    suurem kui / väiksem kui

---
+++ harjutus-6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Harjutus-6</title>
+</head>
+<body> 
+<h1>Loogika operatsioonid</h1>
+<h2>Tingimuslaused</h2>
+
+<?php 
+$a = 4;
+$b = 5;
+$a < $b;
+echo "4 on väiksem kui 5";
+ ?>
+ 
+
+</body>
+</html> 
